fc44624fc533073eda01d1152f9b867da3a5a4dc
CDamasse <charles.damasse@gmail.com>
2021-01-25 19:39:34 +0100

    Un petit pas pour l'homme mais un grand pas pour la breakoutité

--- resources/js/components/rooms/infiniteCristal_components/enigma_2.vue
+++ resources/js/components/rooms/infiniteCristal_components/enigma_2.vue
@@ -1,9 +1,15 @@
 <template>
-    <div class="row justify-content-center">
-        <h1>Enigme 2</h1>
-        <div class="col-md-12">
-            <div>
-                test
+    <div>
+        <div class="row">
+            <div class="col-md-6" id="bassin">
+
+            </div>
+            <div class="col-md-6 align-self-center" style="background-color: #0f6674">
+                <div class="row">
+                    <div class="col-md-4 offset-md-4">
+                        <div id="cristal"></div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -11,10 +17,26 @@
 
 <script>
 export default {
-name: "enigma_2"
+    name: "enigma_2",
+    props: ['room'],
+    data() {
+        return {}
+    },
+
 }
 </script>
 
 <style scoped>
+#bassin {
+    height: 500px;
+    background-color: white;
+}
+
+#cristal {
+    height: 100px;
+    width: 100px;
+    background-color: #bbb;
+    border-radius: 50%;
+}
 
 </style>
